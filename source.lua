337eac9df1632cb2727821a732231096b1fbccc7
Lotux <155517834+Lotux003@users.noreply.github.com>
2024-12-24 00:30:06 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,17 +1,15 @@
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Luxware-UI-Library/main/Source.lua"))()
 
 -- Create Window
-local lib = Library.CreateWindow("Xotux", 6105620301)
+local lib = Library.CreateWindow("Xotux - Fisch", 6105620301)
 
 -- Create Tabs
 local tab1 = lib:Tab("GUIS", 6087485864)
-local tab2 = lib:Tab("tab2")
+--local tab2 = lib:Tab("tab2")
 
+-- Create tab1 section1
+local section1 = tab1:Section("Other Fisch Exploits")
 
-
--- Create tab1 Section
-local section1 = tab1:Section("Fisch Exploit Guis")
 section1:Button("Open Average Hub", function()
     loadstring(game:HttpGet("https://gist.githubusercontent.com/AverageHub/1980eccce4133d77fb24d166dc296125/raw/2d9c88acc21a302d92aed0e8b6f0dcd287c8b96b/gistfile1.txt"))()
 end)
@@ -19,7 +17,9 @@ section1:Button("Open Switch Hub", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/refs/heads/main/Fisch.lua"))()
 end)
 
-local section1 = tab1:Section("Universal Exploit Guis")
+-- Create tab1 section2
+local section1 = tab1:Section("Universal Exploits")
+
 section1:Button("Open Infinite Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end)
@@ -27,7 +27,8 @@ section1:Button("Open Dex Explorer", function()
     loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
 end)
 
---section1:Label("Welcome to Wisteria GUI")
+--[[
+section1:Label("Welcome to Wisteria GUI")
 
 local Examples = lib:Tab("Examples")
 
@@ -52,3 +53,4 @@ end)
 ff:DropDown("Favorite Food?", {"Pizza", "Burger", "Sandwiches"}, function(food) -- food is chosen item
     print(food)
 end)
+]]--
